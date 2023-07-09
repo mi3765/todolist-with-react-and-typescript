96367761f8fdd23333d17c31b4454f960cf2a062
mi3765 <chips1225@outlook.jp>
2023-07-09 16:17:00 +0900

    inputに入力された値を型指定で取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="App">
-
+      <div>
+        <h2>TodoList with Typescript</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text"  className='inputText' onChange={(e) => handleChange(e)}/>
+          <input type="submit" value="作成" className='submitButton'/>
+        </form>
+      </div>
     </div>
   );
 }
