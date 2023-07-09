5da3401a3aa701fe93ed82d50daff809a36954ed
mi3765 <chips1225@outlook.jp>
2023-07-09 16:24:47 +0900

    入力した値を新しく生成したinput要素に追加

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,10 @@ function App() {
       checked: false,
     };
 
+    const handleEdit = () => {
+
+    }
+
     setTodos([newTodo, ...todos]);
     setInputValue("");
   };
@@ -33,9 +37,16 @@ function App() {
       <div>
         <h2>TodoList with Typescript</h2>
         <form onSubmit={(e) => handleSubmit(e)}>
-          <input type="text"  className='inputText' onChange={(e) => handleChange(e)}/>
+          <input type="text" onChange={(e) => handleChange(e)} className='inputText'/>
           <input type="submit" value="作成" className='submitButton'/>
         </form>
+        <ul className='todoList'>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input type="text" onChange={(e) => handleEdit()} className='inputText' value={todo.inputValue}/>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
